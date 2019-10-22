d76825e29757f5a7bc05ec7afcad9dbdcca88043
Damien Cosset <damcossetfreelance@gmail.com>
2019-10-22 13:19:59 +0200

    removed oncreatenode

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,15 +5,4 @@
  */
 
 const { createFilePath } = require(`gatsby-source-filesystem`);
-exports.onCreateNode = ({ node, actions, getNode }) => {
-  const { createNodeField } = actions;
-  console.log(node);
-  if (node.internal.type === `MarkdownRemark`) {
-    const value = createFilePath({ node, getNode });
-    createNodeField({
-      name: `slug`,
-      node,
-      value
-    });
-  }
-};
+exports.onCreateNode = ({ node, actions, getNode }) => {};
